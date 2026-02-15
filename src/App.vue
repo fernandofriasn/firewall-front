9c834c1b0c41221d0358e7348491bc760408c9c4
Fernando Frias <fernandofrias15@gmail.com>
2026-02-14 23:22:31 -0400

    test ci/cd

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -10,7 +10,7 @@
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
+  </header> -->
 
   <div class="ci-banner">🚀 CI/CD TEST — deployed automatically from Jenkins</div>
 
